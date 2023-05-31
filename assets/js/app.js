8f6cfcbc5d2882fea2663e7f2af693399154c329
Manjur-WD <114661279+Manjur-WD@users.noreply.github.com>
2023-05-31 11:43:09 +0530

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+const cookieBox = document.querySelector(".wrapper"),
+  buttons = document.querySelectorAll(".button");
+const executeCodes = () => {
+ 
+  if (document.cookie.includes("nagina_desert_camp")) return;
+  cookieBox.classList.add("show");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      cookieBox.classList.remove("show");
+      
+      if (button.id == "acceptBtn") {
+        //set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
+        document.cookie = "cookieBy= nagina_desert_camp; max-age=" + 60 * 60 * 24 * 30;
+      }
+    });
+  });
+};
+
+window.addEventListener("load", executeCodes);
